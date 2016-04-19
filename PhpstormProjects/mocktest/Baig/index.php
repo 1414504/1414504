b96f8009b5dfbd32aa93f4969d39b2f2db841251
1414504 <a@a.com>
2016-04-19 11:03:08 +0100

    Test

---
+++ PhpstormProjects/mocktest/Baig/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap/js/jquery-1.12.0.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+            <div class="container">
+              <header>  
+                <div class="row">
+                    <div class="col-md-12">
+                        <img class="csdmlogo" src="assets/csdmlogo.png" alt=""/>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="navbar">
+                            <a href="#">Home</a>
+                            <a href="#">About</a>
+                            <a href="#">News</a>
+                            <a href="#">Events</a>
+                            <a href="#">Staff </a>
+                            <a href="#">Research</a>
+                            <a href="#">Study Information</a>
+                            <a href="#">Industry</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12 banner">
+                        <img class="banner-image img-responsive" src="assets/banner.png" alt=""/>
+
+                    </div>
+                </div>
+                  </header>
+                <div class="row">
+                    <div class="col-md-8">
+
+                        <div class="row">
+                            <div class="col-md-5"><img class="cristina" src="assets/chrisinajayne.jpg" alt=""/></div>
+                            <div class="col-md-7">
+                                <h2>Welcome to the School of Computing Science and Digital Media</h2>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </div>
+
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-12">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                                
+                            </div>
+                            
+                        </div>
+                   
+
+                    </div>
+                     <div class="col-md-4">
+                        <div class="row sidebar-1">
+                            <div class="col-md-12">
+                                <h2>Latest News</h2>
+                                <h3>Lorem ipsum dolor</h3>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                       
+                            </div>
+                             </div>
+                         <div class="row sidebar-2">
+                            <div class="col-md-12">
+                                <h2>Up Comming Events</h2>
+                                <h3>Lorem ipsum dolor</h3>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                       
+                            </div>
+                             </div>
+                    </div>
+                    
+
+                </div>
+                <div class="row">
+                    <div  class="col-md-12 footer">
+                        <p>(c) Shahid Baig</p>
+                        
+                        
+                    </div>  
+                </div>
+            </div>
+
+        
+        <main>
+
+        </main>
+        <aside>
+
+        </aside>
+        <footer>
+
+        </footer>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
